2ed58354153308b0acc60a62bb5c56242fbd9064
brunoObarbosa <bruno.barbosa@picpay.com>
2024-12-23 17:15:39 -0300

    feat: add 5 cron

--- src/server.js
+++ src/server.js
@@ -18,32 +18,10 @@ app.use(express.json());
 // Servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-function delay(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-// Enviar dados dinâmicos via WebSocket
-function enviarDadosParaClientes() {
-    const jogos = carregarJogos();
-    const resultados = carregarResultados();
-    const payload = JSON.stringify({ jogos, resultados });
-    wss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send(payload);
-        }
-    });
-}
-
 // Agendar o cron job no servidor para rodar no dia 31/12/2024 às 20h e repetir a cada 5 minutos
 app.get('/api/cron', async (req, res) => {
-    console.log('Executando scraping agendado...');
+    console.log('Executando busca de resultados...');
     await fetchResults(); // Aguarda salvar os dados no arquivo
-
-    console.log('Aguardando 2 segundos para garantir que o arquivo foi salvo...');
-    await delay(2000); // Espera 2 segundos antes de enviar os dados
-
-    console.log('Enviando dados atualizados para os clientes...');
-    enviarDadosParaClientes(); // Notifica os clientes
 });
 
 // Iniciar o servidor HTTP e WebSocket
